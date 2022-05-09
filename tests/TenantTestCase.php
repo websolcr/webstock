2f2b6121f9e397d4484656ee8a5561e6642ffafa
suresh <sureshhemal123@gmail.com>
2022-05-09 19:35:33 +0530

    setting up testing environment inside tenant

--- tests/TenantTestCase.php
+++ tests/TenantTestCase.php
@@ -2,27 +2,48 @@
 
 namespace Tests;
 
+use App\Models\User;
 use App\Models\Tenant;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 abstract class TenantTestCase extends TestCase
 {
     use DatabaseMigrations;
 
-    protected bool $tenancy = false;
-
     public function setUp(): void
     {
         parent::setUp();
 
-        if ($this->tenancy) {
-            $this->initializeTenancy();
-        }
+        $this->initializeTenancy();
+
+        $this->beforeApplicationDestroyed(fn () => tenancy()->query()->get()->each->delete());
     }
 
-    public function initializeTenancy()
+    public function initializeTenancy(): mixed
     {
-        $tenant = Tenant::create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $tenant = Tenant::create([
+            'user_id' => $user->id,
+            'name' => Str::random(),
+        ]);
+
         tenancy()->initialize($tenant);
+        session(['tenant_id' => $tenant->id]);
+
+        return $tenant;
+    }
+
+    public function tearDown(): void
+    {
+        config([
+            'tenancy.queue_database_deletion' => false,
+            'tenancy.delete_database_after_tenant_deletion' => true,
+        ]);
+
+        parent::tearDown();
     }
 }
