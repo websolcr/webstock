55e088bbed2e9fbb204ccd19d30c652858dd6ff6
Suresh Hemal <53757686+sureshhemal@users.noreply.github.com>
2022-05-09 22:16:11 +0530

    implement uuid as default primary key for models (#54)

---
+++ app/Models/AppModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\HasUuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AppModel extends Model
+{
+    use HasUuid;
+    use HasFactory;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]|bool
+     */
+    protected $guarded = [];
+}
--- resources/frontend/src/views/organization/OrganizationCreate.vue
+++ resources/frontend/src/views/organization/OrganizationCreate.vue
@@ -45,7 +45,6 @@ export default {
     BaseInput,
   },
 
-
   data() {
     return {
       form: {
