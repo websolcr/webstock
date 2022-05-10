51ec185c554a7a87dbf1b8c4f92b042cc5cc7544
Suresh Hemal <53757686+sureshhemal@users.noreply.github.com>
2022-05-10 21:23:22 +0530

    create notification widget (#59)
    Co-authored-by: Kushan1994 <63117810+Kushan1994@users.noreply.github.com>

---
+++ resources/frontend/src/components/notification/NotificationWidget.vue
@@ -0,0 +1,150 @@
+<template>
+  <div
+    aria-live="assertive"
+    class="fixed inset-0 flex items-end px-4 pointer-events-none sm:p-6 sm:items-start h-4/3"
+  >
+    <div class="w-full flex flex-col items-center space-y-4 sm:items-end">
+      <transition
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-if="isShowingNotificationWidget"
+          class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden"
+        >
+          <div
+            v-if="notifications.length"
+            class="p-4"
+          >
+            <div class="flex justify-between text-xs">
+              <div>You have {{ notifications.length }} notifications</div>
+              <div>
+                <close-icon
+                  class="cursor-pointer"
+                  @click="$emit('toggleNotificationWidget')"
+                />
+              </div>
+            </div>
+            <ul
+              role="list"
+            >
+              <li
+                v-for="notification in chunkedNotifications[selectedChunkedNotificationIndex]"
+                :key="notification.id"
+                class="relative bg-white py-2 px-4 hover:bg-blue-50 cursor-pointer"
+              >
+                <div class="flex justify-between space-x-3">
+                  <div class="min-w-0 flex-1">
+                    <p class="text-sm font-bold text-gray-900 truncate">
+                      {{ notification.subject }}
+                    </p>
+                  </div>
+                  <time
+                    class="flex-shrink-0 whitespace-nowrap text-xs text-gray-400"
+                  >{{ notification.time }}</time>
+                </div>
+                <div class="mt-1">
+                  <p class="line-clamp-2 text-xs text-gray-500">
+                    {{ notification.preview }}
+                  </p>
+                </div>
+              </li>
+            </ul>
+            <div class="flex justify-between pt-0.5">
+              <button
+                type="button"
+                :disabled="!hasPreviousChunkedNotificationsCollection"
+                :class="[hasPreviousChunkedNotificationsCollection ? activeStyles: disabledStyles]"
+                @click="selectedChunkedNotificationIndex --"
+              >
+                <div class="flex items-center text-xs">
+                  <left-arrow /> Previous
+                </div>
+              </button>
+              <button
+                type="button"
+                :disabled="!hasNextChunkedNotificationsCollection"
+                :class="[hasNextChunkedNotificationsCollection ? activeStyles : disabledStyles]"
+                @click="selectedChunkedNotificationIndex ++"
+              >
+                <div class="flex items-center text-xs">
+                  Next <right-arrow />
+                </div>
+              </button>
+            </div>
+          </div>
+
+          <div
+            v-else
+            class="p-4"
+          >
+            <div class="flex justify-between">
+              <div>You have no notifications</div>
+              <div>
+                <close-icon
+                  class="cursor-pointer"
+                  @click="$emit('toggleNotificationWidget')"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import {chunk} from "lodash"
+import CloseIcon from "@/components/common/svg-icon/CloseIcon"
+import LeftArrow from "@/components/common/svg-icon/LeftArrow"
+import RightArrow from "@/components/common/svg-icon/RightArrow"
+
+export default {
+  name: "NotificationWidget",
+
+  components: {
+    CloseIcon,
+    RightArrow,
+    LeftArrow,
+  },
+
+  props: {
+    isShowingNotificationWidget: {
+      type: Boolean,
+      required: true,
+    },
+
+    notifications: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  data() {
+    return {
+      selectedChunkedNotificationIndex: 0,
+      activeStyles:['text-gray-900', 'cursor-pointer'],
+      disabledStyles:['text-gray-400', 'cursor-not-allowed'],
+    }
+  },
+
+  computed: {
+    chunkedNotifications() {
+      return chunk(this.notifications, 5)
+    },
+
+    hasNextChunkedNotificationsCollection() {
+      return this.selectedChunkedNotificationIndex !== this.chunkedNotifications.length - 1
+    },
+
+    hasPreviousChunkedNotificationsCollection() {
+      return this.selectedChunkedNotificationIndex !== 0
+    }
+  },
+}
+</script>
--- resources/frontend/src/layout/AppLayout.vue
+++ resources/frontend/src/layout/AppLayout.vue
@@ -8,27 +8,63 @@
     </div>
     <div class="flex-1">
       <div>
-        <Navbar />
+        <Navbar
+          :is-showing-notification-widget="isShowingNotificationWidget"
+          @toggleNotificationWidget="toggleNotificationWidget"
+        />
       </div>
       <div class="bg-gray-600 h-screen">
         <router-view :key="$route.fullPath" />
       </div>
     </div>
+
+    <notification-widget
+      v-if="isShowingNotificationWidget"
+      :notifications="notifications"
+      :is-showing-notification-widget="isShowingNotificationWidget"
+      @toggleNotificationWidget="toggleNotificationWidget"
+    />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/layout-components/Navbar"
+import NotificationWidget from "@/components/notification/NotificationWidget"
 
 export default {
   name: 'AppLayout',
+
   components: {
     Navbar,
+    NotificationWidget,
+  },
+
+  data() {
+    return {
+      isShowingNotificationWidget: false,
+      notifications: Array(12).fill({
+        id: 1,
+        subject: 'Velit placeat sit ducimus non sed',
+        time: '1d ago',
+        preview:
+          'Doloremque dolorem maiores assumenda dolorem facilis. Velit vel in a rerum natus facere. Enim rerum eaque qui facilis. Numquam laudantium sed id dolores omnis in. Eos reiciendis deserunt maiores et accusamus quod dolor.',
+      }),
+    }
   },
 
   computed: {
-    hasActiveTenant () {
+    hasActiveTenant() {
       return !!this.$store.state.tenancy
+    },
+
+    organization() {
+      return this.$store.state.tenancy
+    }
+  },
+
+  methods: {
+    toggleNotificationWidget() {
+      this.isShowingNotificationWidget = !this.isShowingNotificationWidget
     },
   }
 }
