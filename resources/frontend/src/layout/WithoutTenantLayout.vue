71e5f81f4f912a3845f177a8f1cb508d65ac7f0b
Kushan1994 <kushan.gayantha@gmail.com>
2022-05-12 21:57:49 +0530

    refactor organization index page

--- resources/frontend/src/layout/WithoutTenantLayout.vue
+++ resources/frontend/src/layout/WithoutTenantLayout.vue
@@ -1,15 +1,54 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="max-w-3xl mx-auto">
+  <div class="bg-blue-50">
+    <div
+      class="bg-indigo-500 shadow flex justify-between h-16 px-6 items-center"
+    >
       <div>
-        <router-view :key="$route.fullPath" />
+        <div class="text-center text-white font-medium">
+          Your Organizations
+        </div>
+      </div>
+      <div>
+        <div class="flex gap-6">
+          <div class="text-sm text-white">
+            {{ user.name }}
+          </div>
+          <div>
+            <svg-icon
+              v-tooltip="'logout'"
+              icon="logout"
+              class="text-white cursor-pointer"
+              @click="logout"
+            />
+          </div>
+        </div>
       </div>
     </div>
+    <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "WithoutTenantLayout"
+  name: "WithoutTenantLayout",
+
+  computed: {
+    user() {
+      return this.$store.state.user
+    },
+  },
+
+  methods: {
+    logout() {
+      this.clearLocalStorage()
+
+      const form = document.getElementById('logout-form')
+      form.submit()
+    },
+    clearLocalStorage() {
+      localStorage.removeItem('user')
+      localStorage.removeItem('tenancy')
+    },
+  }
 }
 </script>
--- resources/frontend/src/views/organization/OrganizationIndex.vue
+++ resources/frontend/src/views/organization/OrganizationIndex.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="flex flex-col">
-    <div class="pb-4 text-center">
-      <div class="font-bold">
-        Your Organizations.
+  <div>
+    <div class="items-center fixed content-center h-screen w-full grid grid-cols-5 px-24 pb-32 pt-8 bg-blue-50">
+      <div class="col-span-3 h-full overflow-y-auto">
+        <div class="shadow sm:rounded-md">
+          <ul
+            role="list"
+            class="divide-y divide-gray-100"
+          >
+            <li
+              v-for="organization in organizations"
+              :key="organization.id"
+              class="bg-white py-5 my-2 hover:bg-blue-100 rounded-lg text-center text-sm cursor-pointer"
+              @click="initializeTenant(organization)"
+            >
+              {{ organization.name }} <br>
+              <span class="text-xs">{{ organization.id }}</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div class="p-6">
-      <div
-        v-for="organization in organizations"
-        :key="organization.id"
-        class="py-5 hover:bg-blue-100 rounded-lg text-center cursor-pointer"
-        @click="initializeTenant(organization)"
-      >
-        {{ organization.name }} <br>
-        <span class="text-xs">{{ organization.id }}</span>
+      <div class="text-center col-span-2">
+        <router-link
+          :to="{name: 'CreateOrganization'}"
+        >
+          <base-button label="Create New Organization" />
+        </router-link>
       </div>
-    </div>
-    <div class="flex justify-center pt-6">
-      <router-link
-        :to="{name: 'CreateOrganization'}"
-      >
-        <base-button label="Create New Organization" />
-      </router-link>
     </div>
   </div>
 </template>
@@ -34,7 +38,6 @@ export default {
   components: {
     BaseButton,
   },
-
 
   data() {
     return {
